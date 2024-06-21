13fd73a7b83ae31637cf0421627f0352c53ad1be
Abdulaziz Ergashev <eabdulaziz36@gmail.com>
2024-06-21 08:51:48 +0500

    First Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [".//*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'client-bg': "url(./images/client-bg-img.svg)",
+        'ready-bg': "url(./images/ready-bg-img.svg)"
+      }
+    },
+  },
+  plugins: [],
+}
+
